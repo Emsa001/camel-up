2b125423d033fb2d746d85a9326ee9a1540f2926
Emanuel <emikscur123@gmail.com>
2025-06-29 03:32:18 +0200

    updates

--- backend/src/main/java/tuc/isse/config/WebConfig.java
+++ backend/src/main/java/tuc/isse/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost", "http://20.52.248.39", "http://camelup.run.place", "https://camelup.emanuelscura.me")
+                .allowedOrigins("https://camelup.emanuelscura.me", "https://www.camelup.emanuelscura.me", "https://camel-up-tan.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
